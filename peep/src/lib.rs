15f0ae2e0f2b450f8b20d9b6c593a3de50211d4f
Wil Gaboury <wgaboury@amazon.com>
2025-07-26 21:16:44 -0600

    Start writing listeneing task

--- peep/src/lib.rs
+++ peep/src/lib.rs
@@ -1,10 +1,10 @@
-use std::{net::{IpAddr, Ipv6Addr}};
+use std::{collections::{HashMap, HashSet}, net::{IpAddr, Ipv6Addr, Shutdown}, sync::{Arc, Mutex}};
 
 use anyhow::anyhow;
 use bootstrap_client::BootstrapClient;
 use bootstrap_common::SessionMemberLocation;
 use local_ip_address::list_afinet_netifas;
-use tokio::{io::AsyncWriteExt, net::TcpListener};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener, select, sync::oneshot};
 
 #[derive(Clone)]
 pub struct PeepClientConfig {
@@ -22,11 +22,35 @@ impl PeepClientConfig {
     }
 }
 
+pub struct PeerState {
+    pub healthy: bool
+}
+
+pub struct PeepClientState {
+    pub peers: HashMap<SessionMemberLocation, PeerState>
+}
+
+type PeepClientStateRef = Arc<Mutex<PeepClientState>>;
+
+pub struct PeepClientInner {
+    pub state: PeepClientStateRef,
+    shutdown_listener: Option<oneshot::Sender<()>>,
+}
+
+impl Drop for PeepClientInner {
+    fn drop(&mut self) {
+        if let Some(shutdown) = std::mem::replace(&mut self.shutdown_listener, None) {
+            shutdown.send(()).unwrap();
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct PeepClient {
     pub session: String,
     pub member: SessionMemberLocation,
-    pub bootstrap: BootstrapClient
+    pub bootstrap: BootstrapClient,
+    pub inner: Arc<PeepClientInner>
 }
 
 impl PeepClient {
@@ -34,7 +58,17 @@ impl PeepClient {
         let session = config.session(&config.bootstrap).await?;
         let (listener, member) = find_inbound_addr(&session, &config.bootstrap).await?;
 
-        Ok(Self { session, member, bootstrap: config.bootstrap })
+        let (shutdown_listener, listener_rx) = oneshot::channel();
+
+        Ok(Self { 
+            session, 
+            member, 
+            bootstrap: config.bootstrap, 
+            inner: Arc::new(PeepClientInner {
+                state: Arc::new(Mutex::new(PeepClientState { peers: HashMap::new() })),
+                shutdown_listener: Some(shutdown_listener)
+            })
+        })
     }
 }
 
@@ -68,4 +102,32 @@ async fn try_create_session_member_location(session: &str, bootstrap_client: &Bo
     stream.shutdown().await?;
                 
     Ok((listener, member)) 
+}
+
+async fn listener_task(listener: TcpListener, mut shutdown: oneshot::Receiver<()>, state: PeepClientStateRef) {
+    loop {
+        select! {
+            (stream, addr) = async { listener.accept().await.unwrap() } => {
+                let state_ref = state.clone();
+                tokio::spawn(async move {
+                    {
+                        let mut locked = state_ref.lock().unwrap();
+                        let location = SessionMemberLocation::try_from(&addr).unwrap();
+                        locked.peers.insert(location, PeerState {
+                            healthy: true
+                        });
+                    };
+
+                    // TODO: do somthing with stream
+
+                    loop {
+                        // let mut buf
+                        // stream.read(buf)
+                    }
+                });
+            },
+            // TODO: figure out how to use oneshot in loop
+            _ = shutdown => return
+        }
+    }
 }
